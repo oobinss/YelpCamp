ea9593162fa106ebd1264039f01e4fbc4ee16c86
oobinss <ohwon99@gmail.com>
2022-12-14 22:08:54 +0900

    맵 추가하기

--- models/campground.js
+++ models/campground.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 const Review = require('./review')
 const Schema = mongoose.Schema
 
+// https://res.cloudinary.com/douqbebwk/image/upload/w_300/v1600113904/YelpCamp/gxgle1ovzd2f3dgcpass.png
+
 const ImageSchema = new Schema({
   url: String,
   filename: String,
@@ -14,6 +16,17 @@ ImageSchema.virtual('thumbnail').get(function () {
 const CampgroundSchema = new Schema({
   title: String,
   images: [ImageSchema],
+  geometry: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
   price: Number,
   description: String,
   location: String,
