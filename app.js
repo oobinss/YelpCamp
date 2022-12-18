83553d7846ccb577a4b98fca47b498f398abb548
oobinss <ohwon99@gmail.com>
2022-12-18 17:59:32 +0900

    Heroku 설정

--- app.js
+++ app.js
@@ -14,14 +14,16 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
 const helmet = require('helmet')
-
 const mongoSanitize = require('express-mongo-sanitize')
-
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const MongoDBStore = require('connect-mongo')(session)
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
@@ -48,10 +50,22 @@ app.use(
     replaceWith: '_',
   })
 )
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
+
+const store = new MongoDBStore({
+  url: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60,
+})
+
+store.on('error', function (e) {
+  console.log('SESSION STORE ERROR', e)
+})
 
 const sessionConfig = {
+  store,
   name: 'session',
-  secret: 'thisshouldbeabettersecret!',
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -142,6 +156,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-  console.log('Serving on port 3000')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`)
 })
